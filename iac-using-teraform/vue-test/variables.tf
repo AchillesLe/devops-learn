fb7a7e6c56eac887cfcc11dbece6896bcda6b87a
AchillesLe <lvbao.cntt.sgu@gmail.com>
2025-05-02 14:37:26 +0700

    feat: remove token

--- iac-using-teraform/vue-test/variables.tf
+++ iac-using-teraform/vue-test/variables.tf
@@ -1,13 +1,13 @@
 variable "GITHUB_TOKEN" {
   description = "GITHUB_TOKEN"
   type        = string
-  default     = "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"
+  default     = "xxxxx"
 }
 
 variable "NPM_TOKEN" {
   description = "NPM_TOKEN"
   type        = string
-  default     = "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"
+  default     = "xx"
 }
 
 variable "GIT_REPO" {
